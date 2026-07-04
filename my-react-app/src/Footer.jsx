5c8eaf40d8026fba34157244830f8a6717dd051a
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-05 00:21:35 +0300

    trying useState hook

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -1,8 +1,32 @@
+import {useState} from 'react'
 
 function Footer() {
+
+	const [number, setNumber] = useState(0);
+
+	const increase = () => {
+		setNumber(number + 1)
+	}
+
+	const reset = () => {
+		setNumber(0)
+	}
+	
+	const decrease = () => {
+		setNumber(number - 1)
+	}
+	
+	
 	return (
-		<footer className="flex bottom-0 bg-amber-200 p-2 ">
+		<footer className="flex flex-row justify-between bottom-0 bg-amber-200 pl-2 ">
 			<p className="text-xl font-medium italic">&copy; {new Date().getFullYear()} Hello, world! website</p>
+			
+			<div className='bg-amber-400 w-fit h-full '>
+				<h1 className='text-4xl text-center'>{number}</h1>
+				<button className='text-2xl mx-1 p-1' onClick={increase}>+</button>
+				<button className='text-2xl mx-1 p-1' onClick={reset}>reset</button>
+				<button className='text-2xl mx-1 p-1' onClick={decrease}>-</button>
+			</div>
 		</footer>
 	)
 }

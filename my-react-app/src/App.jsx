6374def92fd325b961887841ca826dbef2bca530
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-04 22:12:38 +0300

    add some props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -11,10 +11,9 @@ function App() {
     <Header/>
 
     <div className='flex flex-row justify-around flex-wrap'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card name='Kirill' age={21} topic={'Learning react and searching for a job'} showAge={true}/>
+      <Card name='Nikita' age={21} topic={'Driving the truck'} showAge={true}/>
+      <Card name='Vadim' age={22} topic={'Writing samp bots'} showAge={false}/>
     </div>
     
     <Footer/>
--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,14 +1,15 @@
 import profilePic from './assets/card.jpg'
 
-function Card() {
+function Card({name, age, topic, showAge}) {
+	showAge !== true ? age='': age
 	return (
-		<div className='my-9 mx-5 bg-amber-200 w-1/5 flex flex-col flex-wrap items-center text-center rounded-2xl shadow-xl max-w-60 min-w-40'>
+		<div className='my-9 mx-5 bg-amber-200 w-1/5 flex flex-col  items-center text-center rounded-2xl shadow-xl max-w-60 min-w-40'>
 
 			<img src={profilePic}  alt="default profile" className='p-2 w-[60%] h-auto rounded-full'></img>
+			
+			<h3 className='font-sans font-bold text-2xl'>{name} {age}</h3>
 
-			<h3 className='font-sans font-bold text-2xl'>Kirill Sezen</h3>
-
-			<p className='font-sans font-medium text'>Learning react and searching for a job</p>
+			<p className='font-sans font-medium text'>{topic}</p>
 
 		</div>
 	)

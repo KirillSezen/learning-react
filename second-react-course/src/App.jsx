99560583c3a59122168b094e845ee5b9f671c648
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-10 23:46:26 +0300

    restyle buttons

--- second-react-course/src/App.jsx
+++ second-react-course/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
 
       <div className="flex flex-row justify-around">
 
-        <button onClick={() => setCount(count + 1)} className="p-2 border-2">+</button>
+        <button onClick={() => setCount(count + 1)} className="p-2 border-none bg-teal-400 rounded-full w-20 h-auto font-medium text-4xl">+</button>
 
-        <button onClick={() => setCount(0)} className="p-2 border-2">reset</button>
+        <button onClick={() => setCount(0)} className="p-4 border-none bg-teal-400 rounded-full w-fit h-auto font-medium text-4xl">reset</button>
 
-        <button onClick={() => setCount(count - 1)} className="p-2 border-2">-</button>
+        <button onClick={() => setCount(count - 1)} className="p-2 border-none bg-teal-400 rounded-full w-20 h-auto font-medium text-4xl">-</button>
 
       </div>
     </div>

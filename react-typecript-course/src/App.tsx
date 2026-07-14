6851f6f21fffb4d160a3499e05ba18f916048c4a
Kirill Sezen <kirill_sezen@mail.ru>
2026-07-14 23:58:30 +0300

    add interface CardVariant

--- react-typecript-course/src/App.tsx
+++ react-typecript-course/src/App.tsx
@@ -1,8 +1,17 @@
+import Card, { CardVariant } from "./components/Card"
 
 function App() {
 
   return (
-    <h1>Hello, TypeScript!</h1>
+    <>
+
+      <h1>Hello, TypeScript!</h1>
+      <Card width="256px" height="25px" variant={CardVariant.outlined}>
+        <button>Кнопка</button>
+      </Card>
+
+    </>
+   
   )
 }
 
